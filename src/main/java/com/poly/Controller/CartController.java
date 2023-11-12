2980956e0dd9b2e78c1485bc54897c58304db28d
lechibang2003 <lechibangcmu15@gmail.com>
2023-11-13 02:01:25 +0700

    update admin,product, validate form,img cloud, API cut bg ảnh

--- src/main/java/com/poly/Controller/CartController.java
+++ src/main/java/com/poly/Controller/CartController.java
@@ -1,6 +1,8 @@
 package com.poly.Controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.poly.Entity.*;
 import com.poly.Reponsitory.*;
@@ -40,27 +42,27 @@ public class CartController {
     private SizeRepository sizeRepo;
 
     @Autowired
-    private  SizeProductRepository sizeProductRepo;
+    private SizeProductRepository sizeProductRepo;
 
     @GetMapping("/cart")
     public String listProducts(Model model, HttpServletRequest httpServletRequest) {
         String username = httpServletRequest.getRemoteUser();
         Carts carts = cartRepo.findByCartUser(username);
-        model.addAttribute("carts",carts);
-        Long subtotal= cartItemsRepo.getSum(carts.getCartID());
-        model.addAttribute("subtotal",subtotal);
+        model.addAttribute("carts", carts);
+        Long subtotal = cartItemsRepo.getSum(carts.getCartID());
+        model.addAttribute("subtotal", subtotal);
 
         return "user/cart";
     }
 
     @PostMapping("/addToCart/{productId}")
     public String addToCart(@PathVariable int productId, Model model, HttpServletRequest httpServletRequest,
-                            @RequestParam("size") String size, @RequestParam("soluong") int soluong) {
+            @RequestParam("size") String size, @RequestParam("soluong") int soluong) {
         String username = httpServletRequest.getRemoteUser();
         Account account = accountRepo.findByUsername(username);
         Carts cart = cartRepo.findByCartsUsername(account.getAccountID());
         Products product = productRepo.findByProduct(productId);
-        Size size1 = sizeRepo.findByProductAndSizeName(productId,size);
+        Size size1 = sizeRepo.findByProductAndSizeName(productId, size);
         Size_Product sizeProduct = sizeProductRepo.findBySizeProductId(size1.getSizeID());
         if (cart == null || cart.getCart_items().isEmpty()) {
             Carts newCart = new Carts();
@@ -76,74 +78,95 @@ public class CartController {
             cartItems.setSizeName(size);
             cartItemsRepo.save(cartItems);
             return "redirect:/cart";
-        }
-        else {
-            Cart_Items cart_Items = cartItemsRepo.findByProductAndSize(productId,size);
-            if(cart_Items!=null){
+        } else {
+            Cart_Items cart_Items = cartItemsRepo.findByProductAndSize(productId, size);
+            if (cart_Items != null) {
                 Integer product1 = cart_Items.getProductId().getProductId();
                 String size2 = cart_Items.getSizeName();
 
-                    cart_Items.setQuantity(cart_Items.getQuantity()+soluong);
-                    float quantity = cart_Items.getQuantity();
-                    cart_Items.setSubtotal((float) (quantity * cart_Items.getPrice()));
-                    cartItemsRepo.save(cart_Items);
+                cart_Items.setQuantity(cart_Items.getQuantity() + soluong);
+                float quantity = cart_Items.getQuantity();
+                cart_Items.setSubtotal((float) (quantity * cart_Items.getPrice()));
+                cartItemsRepo.save(cart_Items);
 
-                }else {
-                    Cart_Items cartItems = new Cart_Items();
-                    cartItems.setProductId(product);
-                    cartItems.setCarts(cart);
-                    cartItems.setQuantity(soluong);
-                    cartItems.setPrice(sizeProduct.getPrice());
-                    cartItems.setSubtotal(soluong * sizeProduct.getPrice());
-                    cartItems.setSizeName(size);
-                    cartItemsRepo.save(cartItems);
-                }
-
-
-            return "redirect:/cart";
+            } else {
+                Cart_Items cartItems = new Cart_Items();
+                cartItems.setProductId(product);
+                cartItems.setCarts(cart);
+                cartItems.setQuantity(soluong);
+                cartItems.setPrice(sizeProduct.getPrice());
+                cartItems.setSubtotal(soluong * sizeProduct.getPrice());
+                cartItems.setSizeName(size);
+                cartItemsRepo.save(cartItems);
             }
 
+            return "redirect:/cart";
+        }
+
     }
-    
+
     @GetMapping("cart/remove/{cartitemID}")
-    public String remove(@PathVariable("cartitemID") Long Id){
+    public String remove(@PathVariable("cartitemID") Long Id) {
         Cart_Items cartItems = cartItemsRepo.findByCartitemID(Id);
         cartItemsRepo.delete(cartItems);
         return "redirect:/cart";
     }
+
     @GetMapping("cart/updateCartItems")
-    public String updateQuantity( @RequestParam(value = "cartitemID",defaultValue = "") long cartitemID,
-                                  HttpServletRequest httpServletRequest, Model model){
+    public String updateQuantity(@RequestParam(value = "cartitemID", defaultValue = "") long cartitemID,
+            HttpServletRequest httpServletRequest, Model model) {
+
         Cart_Items cartItem = cartItemsRepo.findByCartitemID(cartitemID);
         String username = httpServletRequest.getRemoteUser();
         Carts carts = cartRepo.findByCartUser(username);
-        model.addAttribute("carts",carts);
-        Long subtotal= cartItemsRepo.getSum(carts.getCartID());
-        model.addAttribute("subtotal",subtotal);
-        int soluong = cartItem.getQuantity()-1;
+        model.addAttribute("carts", carts);
+
+        Long subtotal = cartItemsRepo.getSum(carts.getCartID());
+        model.addAttribute("subtotal", subtotal);
+
+        int soluong = cartItem.getQuantity() - 1;
         cartItem.setQuantity(soluong);
-        cartItem.setSubtotal(cartItem.getPrice()*soluong);
+        cartItem.setSubtotal(cartItem.getPrice() * soluong);
 
         cartItemsRepo.save(cartItem);
         return "user/cart";
     }
+
     @GetMapping("cart/updateCartItemss")
-    public String updateQuantityAdd( @RequestParam(value = "cartitemID",defaultValue = "") long cartitemID,
-                                     HttpServletRequest httpServletRequest, Model model){
+    public String updateQuantityAdd(@RequestParam(value = "cartitemID", defaultValue = "") long cartitemID,
+            HttpServletRequest httpServletRequest, Model model) {
         Cart_Items cartItem = cartItemsRepo.findByCartitemID(cartitemID);
         String username = httpServletRequest.getRemoteUser();
         Carts carts = cartRepo.findByCartUser(username);
-        model.addAttribute("carts",carts);
-        Long subtotal= cartItemsRepo.getSum(carts.getCartID());
-        model.addAttribute("subtotal",subtotal);
-        int soluong = cartItem.getQuantity()+1;
+        model.addAttribute("carts", carts);
+
+        Long subtotal = cartItemsRepo.getSum(carts.getCartID());
+        model.addAttribute("subtotal", subtotal);
+
+        int soluong = cartItem.getQuantity() + 1;
         cartItem.setQuantity(soluong);
-        cartItem.setSubtotal(cartItem.getPrice()*soluong);
+        cartItem.setSubtotal(cartItem.getPrice() * soluong);
+
         cartItemsRepo.save(cartItem);
         return "user/cart";
     }
 
+    // @GetMapping("/cart/getTotalPrice")
+    // public Map<String, Object> getTotalPrice(HttpServletRequest
+    // httpServletRequest) {
+    // Map<String, Object> response = new HashMap<>();
+    // try {
+    // String username = httpServletRequest.getRemoteUser();
+    // Carts carts = cartRepo.findByCartUser(username);
+    // Long totalPrice = cartItemsRepo.getSum(carts.getCartID()); // Implement this
+    // method in your
+    // // repository
+    // response.put("totalPrice", totalPrice);
+    // } catch (Exception e) {
+    // response.put("error", "Error calculating total price");
+    // }
 
-
+    // return response;
+    // }
 
 }
