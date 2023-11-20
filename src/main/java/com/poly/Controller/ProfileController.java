b83b3994d7196f1b7cd81d9c87facfa2193891a7
ngovanlien <134181190+ngovanlien@users.noreply.github.com>
2023-11-20 13:28:53 +0700

    thêm login gg

--- src/main/java/com/poly/Controller/ProfileController.java
+++ src/main/java/com/poly/Controller/ProfileController.java
@@ -7,13 +7,16 @@ import com.poly.Service.AccountService;
 import com.poly.Service.CustomOAuth2User;
 import com.poly.Service.UserServiceGoogle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -34,17 +37,25 @@ public class ProfileController {
     PasswordEncoder passwordEncoder;
     @Autowired
     UserServiceGoogle userServiceGoogle;
-    @RequestMapping()
-    public String account(HttpServletRequest request, OAuth2AuthenticationToken token,Model model){
-        String username=request.getRemoteUser();
-        Account account=accountService.findByUsername(username);
+    @RequestMapping
+    public String account(Model model,
+                          Authentication authentication){
+        String users = "";
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+            OAuth2User user = oauthToken.getPrincipal();
+            users = user.getAttribute("email");
+            System.out.println(1);
+        } else if (authentication instanceof UsernamePasswordAuthenticationToken) {
+            users = authentication.getName();
+            System.out.println(2);
+        }
+        Account account=accountService.findByUsername(users);
         model.addAttribute("account", new Account());
         model.addAttribute("profile", account);
-        OAuth2User user = token.getPrincipal();
-        String email = user.getAttribute("email");
         return "user/profile";
     }
-    @PostMapping()
+    @PostMapping
     public String changepass(Model m,HttpServletRequest request,
         @ModelAttribute("account") Account account){
         String username=request.getRemoteUser();
