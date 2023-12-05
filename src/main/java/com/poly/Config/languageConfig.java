fbfed10d0e2a56fda29ef83370ee044ae703bdd1
ngovanlien <134181190+ngovanlien@users.noreply.github.com>
2023-12-05 23:47:02 +0700

    uodate language

--- src/main/java/com/poly/Config/languageConfig.java
+++ src/main/java/com/poly/Config/languageConfig.java
@@ -1,43 +1,43 @@
-package com.poly.Config;
-import java.util.Locale;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-@Configuration
-public class languageConfig implements WebMvcConfigurer {
-
-
-    @Bean(name = "localeResolver")
-    public LocaleResolver getLocaleResolver()  {
-        CookieLocaleResolver resolver= new CookieLocaleResolver();
-        resolver.setCookieDomain("myAppLocaleCookie");
-        resolver.setDefaultLocale(Locale.ENGLISH);
-        // 60 minutes 
-        resolver.setCookieMaxAge(60*60);
-        return resolver;
-    }
-
-    @Bean(name = "messageSource")
-    public MessageSource getMessageResource()  {
-        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
-
-        // Đọc vào file i18n/messages_xxx.properties
-        // Ví dụ: i18n/messages_en.properties
-        messageResource.setBasename("classpath:i18n/messages");
-        messageResource.setDefaultEncoding("UTF-8");
-        return messageResource;
-    }
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
-        localeInterceptor.setParamName("language");
-        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
-    }
-
-}
+//package com.poly.Config;
+//import java.util.Locale;
+//import org.springframework.context.MessageSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+//import org.springframework.web.servlet.LocaleResolver;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+//import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+//@Configuration
+//public class languageConfig implements WebMvcConfigurer {
+//
+//
+//    @Bean(name = "localeResolver")
+//    public LocaleResolver getLocaleResolver()  {
+//        CookieLocaleResolver resolver= new CookieLocaleResolver();
+//        resolver.setCookieDomain("myAppLocaleCookie");
+//        resolver.setDefaultLocale(Locale.ENGLISH);
+//        // 60 minutes
+//        resolver.setCookieMaxAge(60*60);
+//        return resolver;
+//    }
+//
+//    @Bean(name = "messageSource")
+//    public MessageSource getMessageResource()  {
+//        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
+//
+//        // Đọc vào file i18n/messages_xxx.properties
+//        // Ví dụ: i18n/messages_en.properties
+//        messageResource.setBasename("classpath:i18n/messages");
+//        messageResource.setDefaultEncoding("UTF-8");
+//        return messageResource;
+//    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+//        localeInterceptor.setParamName("language");
+//        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+//    }
+//
+//}
